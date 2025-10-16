5fb4aa3e4a6617e3548f6d52ca72365aa4f152cc
inshikchoo-hue <inshikchoo@gmail.com>
2025-10-16 12:19:43 +0900

    GNB 수정
    메뉴 스크립트 적용

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 // 메뉴
+$(function () {
+    $(".menu").on("mouseenter", function () {
+        $(this).find(".sub-menu-list").stop().slideDown(300);
+    });
+    $(".menu").on("mouseleave", function () {
+        $(this).find(".sub-menu-list").stop().slideUp(300);
+    });
+});
